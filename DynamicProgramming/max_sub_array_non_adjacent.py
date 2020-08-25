d4fe4ebca561e3d3c2ad727fa7cbd0be895dba5e
Max Thorpe <max.thorpe@accenture.com>
2020-08-25 20:30:27 +0100

    second to solution to max no adjacent subarray problem

--- DynamicProgramming/max_sub_array_non_adjacent.py
+++ DynamicProgramming/max_sub_array_non_adjacent.py
@@ -1,7 +1,16 @@
 def maxSubsetSum(arr):
-    n_1 = 0
-    n = 0
+    dp_n_1 = 0
+    dp_n_2 = 0
+    dp_n = 0
 
+    for n in arr:
+        dp_n = max([dp_n_1, dp_n_2, dp_n_2 + n ])
+        dp_n_2 = dp_n_1
+        dp_n_1 = dp_n
+
+    return dp_n
+
+def maxSubsetSum_2(arr):
     incl = 0
     excl = 0
      
