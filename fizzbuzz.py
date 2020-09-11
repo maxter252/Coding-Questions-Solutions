36ed96e8e95d3e251c054363dff0ab21f850f081
Max Thorpe <max.thorpe@accenture.com>
2020-09-11 19:13:51 +0100

    better fizz buzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -5,7 +5,7 @@
 # 7 : foo
 # 11 : bar
 
-def fizzbuzz():
+def fizzbuzzSimple():
     for i in range(1, 101):
         if (i % 5 ) == 0 and (i % 3) == 0:
             print ("fizzbuzz")
@@ -16,5 +16,28 @@ def fizzbuzz():
         else:
             print(i)
 
+# fizzbuzzSimple()
 
-fizzbuzz()
+def fizzbuzzParam(n, conditions):
+    for i in range(1, 101):
+        output = ''
+        for condition in conditions:
+            if (i % condition[1]) == 0:
+                output += condition[0]
+        print(i, output)
+
+
+# fizzbuzzParam(100, [("fizz",3),("buzz",5)])
+
+def fizzbuzzwizz(n, conditions):
+    for i in range(1, 101):
+        output = ''
+        for condition in conditions:
+            if condition[1](i):
+                output += condition[0]
+        print(i, output)
+
+fizzbuzzwizz(100, [
+    ("fizz",lambda a : (a % 3) == 0),
+    ("buzz",lambda a : (a % 5) == 0),
+    ("wizz",lambda a : a > 90)])
